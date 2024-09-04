f4d759b54df880a5892b9261c525250072a3ee95
Rafael Toresani <rtoresani@gmail.com>
2024-09-04 14:03:40 -0300

    add: dashboard routes

--- src/app/pages/management-page/management-page.component.ts
+++ src/app/pages/management-page/management-page.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-management-page',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './management-page.component.html',
   styleUrl: './management-page.component.css'
 })
 export class ManagementPageComponent {
 
+  constructor(private router:Router){
+  }
+
+  navigateTo(url:String){
+    this.router.navigateByUrl(`/management/${url}`)
+  }
 }
